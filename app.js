e91775748e927e588898e18a58605b8c9501d86d
Berke Sayın <sayinberkesayin@gmail.com>
2023-03-21 21:33:54 +0300

    Add register function

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 // JavaScript
 
+
+// login
 const loginUser = () => {
     console.log('User Login is Successful');
 }
-
 loginUser();
+
+// register
+const registerUser = () => {
+    console.log('User Registration is Successful');
+}
+registerUser();
